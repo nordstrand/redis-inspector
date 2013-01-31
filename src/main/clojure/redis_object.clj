6a07d7b8a2759bf8a9d5e6ac264245ba374e4fb5
jonte <jonas.nordstrand@gmail.com>
2013-01-31 21:45:27 +0100

    basic support for more types

--- src/main/clojure/redis_object.clj
+++ src/main/clojure/redis_object.clj
@@ -25,6 +25,19 @@
  ;  :renderer :bootstrap-stacked
    })
 
+(def list-form
+  { 
+   :submit-label "OK"
+   :method :get
+   :fields [{:name :h4 :type :heading :text "Show elements"}
+            {:name :from :datatype :int}
+            {:name :to :datatype :int}
+            ]
+   :validations [[:required [:from :to]]
+                 [:int [:from :to]]
+                ; [:boolean [:reverse]]
+                 ]
+   })
 (def hash-form
   { 
    :submit-label "OK"
@@ -49,7 +62,7 @@
 
 
 
-(declare redis-show-zset redis-show-hash)
+(declare redis-show-zset redis-show-hash redis-show-string redis-show-list redis-show-set)
 
 (defn redis-show-object [name key & params]
   (let [type (redis-tools/winstance (get @redis-tools/redises name) (car/type key))]
@@ -57,13 +70,16 @@
     (= type "zset" ) (redis-show-zset name key (first params))
     (= type "hash" ) (redis-show-hash name key (first params))
     (= type "string" ) (redis-show-string name key (first params))
-    true (str "Object " key " of type " type "not yet supported."))))
+    (= type "list" ) (redis-show-list name key (first params))
+    (= type "set" ) (redis-show-set name key (first params))
+    true (str "Object " key " of type " type " not yet supported."))))
 
-(defn redis-show-string [name key & params]
+
+(defn redis-show-hash [name key & params]
   (let [defaults {}
         values (merge defaults (first params))
+        size (redis-tools/winstance (get @redis-tools/redises name) (car/hlen key))
         base-url (format "/redis/%s/%s" name key)
-        size (redis-tools/winstance (get @redis-tools/redises name) (car/strlen key))
         instance (get @redis-tools/redises name)
         hkey (-> values :hkey)]
     (layout
@@ -74,25 +90,47 @@
          [:th "Key"]
          [:th "Value"]] 
         [:tr
-         [:td key]
-        [:td (when (= "value" (:show values)) (redis-tools/winstance instance (car/get key)))]
+         [:td (:hkey values)]
+         [:td (redis-tools/winstance instance (when hkey (car/hget key hkey)))]]
         ]
-       ]]
+
+       ]
       [:div.pull-right {:style "width: 43%"}
            [:h4 key]          
           [:ul
            [:li "Type: " (redis-tools/winstance instance (car/type key))]
            [:li "Size: " size]
           ]]
-      (f/render-form (assoc string-form    
+      (f/render-form (assoc hash-form    
                             :action base-url
                             :values values)
                              ))))
+
+
+(defn redis-show-set [name key & params]
+  (let [defaults {}
+        values (merge defaults (first params))
+        base-url (format "/redis/%s/%s" name key)
+        size (redis-tools/winstance (get @redis-tools/redises name) (car/scard key))
+        instance (get @redis-tools/redises name)
+        hkey (-> values :hkey)]
+    (layout
+      (breadcrumb name key hkey)     
+      [:div.pull-left {:style "width: 55%"}
+       ]
+      [:div.pull-right {:style "width: 43%"}
+           [:h4 key]          
+          [:ul
+           [:li "Type: " (redis-tools/winstance instance (car/type key))]
+           [:li "Size: " size]
+          ]]
+      )))
 
 (defn redis-show-hash [name key & params]
   (let [defaults {}
         values (merge defaults (first params))
         size (redis-tools/winstance (get @redis-tools/redises name) (car/hlen key))
+        base-url (format "/redis/%s/%s" name key)
         instance (get @redis-tools/redises name)
         hkey (-> values :hkey)]
     (layout
@@ -154,6 +192,43 @@
            [:li "From: " (:from values) " To: " (:to values)]
           ]]
       (f/render-form (assoc zset-form    
+                            :action base-url
+                            :values values)
+                             ))))
+  
+
+
+(defn redis-show-list [name key & params]
+  (let [defaults {:from 0, :to 10}
+        values (merge defaults (first params))
+        from (bigint (:from values)) 
+        to (bigint (:to values)) 
+        base-url (format "/redis/%s/%s" name key)
+        size (redis-tools/winstance (get @redis-tools/redises name) (car/llen key))
+        instance (get @redis-tools/redises name)]
+    (layout
+      (breadcrumb name key)
+      [:div.pull-left {:style "width: 55%"}
+       (paginate base-url from to size) 
+       [:table.table.table-bordered
+        [:tr
+         [:th "Value"]]
+        (for [value  (redis-tools/winstance instance (car/lrange key (:from values) (:to values)))]
+          [:tr
+           [:td value]]
+          )
+        ]
+       (paginate base-url from to size) 
+       ]
+      [:div.pull-right {:style "width: 43%"}
+           [:h4 key]
+           
+          [:ul
+           [:li "Type: " (redis-tools/winstance instance (car/type key))]
+           [:li "Size: " size]
+           [:li "From: " (:from values) " To: " (:to values)]
+          ]]
+      (f/render-form (assoc list-form    
                             :action base-url
                             :values values)
                              ))))
